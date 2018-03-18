9fac5aadd87b29ac9a1fb4afe550ff82c1953012
LucasMMBB <maoxummbb@gmail.com>
2018-03-18 14:57:51 -0400

    optimized file zeros.cpp

--- course-projects/histogram-filter-improved/zeros.cpp
+++ course-projects/histogram-filter-improved/zeros.cpp
@@ -3,20 +3,24 @@
 using namespace std;
 
 vector < vector <float> > zeros(int height, int width) {
-	int i, j;
+	int row, col;
   
 	// OPTIMIZATION: Reserve space in memory for vectors
 	vector < vector <float> > newGrid;
 	vector <float> newRow;
 
+	newGrid.reserve(height);
+	newRow.reserve(width);
+
   	// OPTIMIZATION: nested for loop not needed
     // because every row in the matrix is exactly the same
-	for (i=0; i<height; i++) {
-		newRow.clear();
-		for (j=0; j<width; j++) {
-			newRow.push_back(0.0);
-		}
+	for (col = 0; col < width; col++) {
+		newRow.push_back(0.0);
+	}
+	for (row = 0; row < height; row++) {
 		newGrid.push_back(newRow);
 	}
 	return newGrid;
+
+	// Optimized time: from O(m*n) to O(m+n)
 }
