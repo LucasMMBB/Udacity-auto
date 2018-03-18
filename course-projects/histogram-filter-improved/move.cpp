3384d6ee5bf8043e34a59d086b922cd87fb5a200
LucasMMBB <maoxummbb@gmail.com>
2018-03-18 16:30:46 -0400

    optimize move.cpp

--- course-projects/histogram-filter-improved/move.cpp
+++ course-projects/histogram-filter-improved/move.cpp
@@ -5,29 +5,21 @@ using namespace std;
 
 // OPTIMIZATION: Pass large variable by reference
 vector< vector <float> > move(int dy, int dx, 
-	vector < vector <float> > beliefs) 
+	vector < vector <float> > &beliefs) 
 {
-	int height, width;
+	int height, width, i, j;
 	height = beliefs.size();
 	width = beliefs[0].size();
 
-	float belief;
 	vector < vector <float> > newGrid;
   
   	// OPTIMIZATION: Use improved zeros function
 	newGrid = zeros(height, width);
 
-// OPTIMIZATION: Eliminate any variables that aren't needed
-	vector <float> row, newRow;
-	int i, j, new_i, new_j;
-	
+        // OPTIMIZATION: Eliminate any variables that aren't needed
   	for (i=0; i<height; i++) {
 		for (j=0; j<width; j++) {
-			new_i = (i + dy + height) % height;
-			new_j = (j + dx + width)  % width;
-			belief = beliefs[i][j];
-
-			newGrid[new_i][new_j] = belief;
+			newGrid[(i + dy + height) % height][(j + dx + width) % width] = beliefs[i][j];
 		}
 	}
 	return newGrid;
