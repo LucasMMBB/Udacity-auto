9f4ffa157b812fdc7131938a4baf81320320fd77
LucasMMBB <maoxummbb@gmail.com>
2018-03-18 15:45:13 -0400

    optimize sense.cpp

--- course-projects/histogram-filter-improved/sense.cpp
+++ course-projects/histogram-filter-improved/sense.cpp
@@ -3,38 +3,28 @@
 using namespace std;
 
 // OPTIMIZATION: Pass larger variables by reference
-vector< vector <float> > sense(char color, vector< vector <char> > grid, vector< vector <float> > beliefs,  float p_hit, float p_miss) 
+vector< vector <float> > sense(char color, vector< vector <char> > &grid, vector< vector <float> > &beliefs,  float p_hit, float p_miss) 
 {
 	// OPTIMIZATION: Is the newGrid variable necessary?
   	// Could the beliefs input variable be updated directly?
-  	vector< vector <float> > newGrid;
-	vector<float> row, newRow;
-
-	float prior, p;
+	float p;
 
 	char cell;
 
-	int i, j, height, width;
-	height = grid.size();
-	width = grid.size();
+	int i, j;
 
 	for (i=0; i<grid.size(); i++) {
-		newRow.clear();
 		for (j=0; j<grid[0].size(); j++) {
-          	// OPTIMIZATION: Which of these variables are 					needed?
-			prior = beliefs[i][j];
-			cell = grid[i][j];
-			if (cell == color) {
-				p = prior * p_hit;
+          	// OPTIMIZATION: Which of these variables are needed?
+			if (grid[i][j] == beliefs[i][j]) {
+				p = beliefs[i][j] * p_hit;
 			}
-            // OPTIMIZATION: if else statements might be 
-          	// 	faster than two if statements
-			if (cell != color) {
-				p = prior * p_miss;
+                // OPTIMIZATION: if else statements might be faster than two if statements
+			else {
+				p = beliefs[i][j] * p_miss;
 			}
-			newRow.push_back(p);
+                        beliefs[i][j] = p;
 		}
-		newGrid.push_back(newRow);
 	}
-	return newGrid;
+	return beliefs;
 }
