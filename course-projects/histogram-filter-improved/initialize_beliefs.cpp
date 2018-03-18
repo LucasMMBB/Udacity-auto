24790daf1ea4b759a5dd05d67cf79a16ba9e9b50
LucasMMBB <maoxummbb@gmail.com>
2018-03-18 15:28:48 -0400

    optimize initialize_beliefs.h

--- course-projects/histogram-filter-improved/initialize_beliefs.cpp
+++ course-projects/histogram-filter-improved/initialize_beliefs.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 // OPTIMIZATION: pass large variables by reference
-vector< vector <float> > initialize_beliefs(vector< vector <char> > grid) {
+vector< vector <float> > initialize_beliefs(vector< vector <char> > &grid) {
 
 	// OPTIMIZATION: Which of these variables are necessary?
 	// OPTIMIZATION: Reserve space in memory for vectors
   	vector< vector <float> > newGrid;
 	vector<float> newRow;
-	int i, j, height, width, area;
+	int i, j, height, width;
 	float total, prob_per_cell;
 
 	height = grid.size();
 	width = grid[0].size();
- 
-	area = height * width;
 
-  	prob_per_cell = 1.0 / ( (float) area) ;
+        newGrid.reserve(height);
+        newRow.reserve(width);
+ 
+  	prob_per_cell = 1.0 / ( (float) height*width) ;
 
   	// OPTIMIZATION: Is there a way to get the same results 	// without nested for loops?
-	for (i=0; i<grid.size(); i++) {
-		newRow.clear();
-		for (j=0; j<grid[0].size(); j++) {
-			newRow.push_back(prob_per_cell);
-		}
-		newGrid.push_back(newRow);
-	}
+        for (i = 0; i < width; i++) {
+            newRow.push_back(prob_per_cell);
+        }
+        for (j = 0; j < height; j++) {
+            newGrid.push_back(newRow);
+        }
 	return newGrid;
 }
